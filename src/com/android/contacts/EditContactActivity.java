da73e9ba24ef3985b999b7a5454af07ec4106a62
Evan JIANG <firstfan@gmail.com>
2008-11-02 10:57:59 +0800

    Fix issue 1167: Contacts: adding new Organization entry erases "position"s fields of others
    http://code.google.com/p/android/issues/detail?id=1167
    The text in data2 textview is not saved before.

--- src/com/android/contacts/EditContactActivity.java
+++ src/com/android/contacts/EditContactActivity.java
@@ -1424,6 +1424,13 @@ public final class EditContactActivity extends Activity implements View.OnClickL
             if (!TextUtils.isEmpty(enteredText)) {
                 entry.data = enteredText;
             }
+            TextView data2 = (TextView) entry.view.findViewById(R.id.data2);
+            if (data2 != null) {
+                String enteredText2 = data2.getText().toString();
+                if (!TextUtils.isEmpty(enteredText2)) {
+                    entry.data2 = enteredText2;
+                }
+            }
         }
 
         // Build a new view
@@ -1458,6 +1465,9 @@ public final class EditContactActivity extends Activity implements View.OnClickL
             if (entry.syncDataWithView) {
                 // If there is already data entered don't overwrite it
                 data.setText(entry.data);
+                if (data2 != null) {
+                    data2.setText(entry.data2);
+                }
             } else {
                 fillViewData(entry);
             }
