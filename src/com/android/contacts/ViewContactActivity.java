c674558dfdb8b7c4da539df3c86be5244e51c0fb
L&T Infotech <androidlntinfotech@gmail.com>
2010-05-12 13:55:04 +0530

    Defect 7926 : Fixed to view notes data fully
    Change-Id: Ic7caa592d0a9e20ac1e08afbbd891118b3722b4b

--- src/com/android/contacts/ViewContactActivity.java
+++ src/com/android/contacts/ViewContactActivity.java
@@ -1237,7 +1237,6 @@ public class ViewContactActivity extends Activity
                 textView.setEllipsize(TextUtils.TruncateAt.END);
             } else {
                 textView.setSingleLine(false);
-                textView.setMaxLines(maxLines);
                 textView.setEllipsize(null);
             }
         }
