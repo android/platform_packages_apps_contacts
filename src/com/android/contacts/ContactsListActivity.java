27a67d599d339753ef8d2f2ae8aa17c872b14852
L&T Infotech <androidlntinfotech@gmail.com>
2010-05-13 15:17:20 +0530

    Defect 7261: Edit number before call in ContactsListActivty
    Change-Id: Ibc70e2a5005a1e5e011f3a1c735950aab9a6994f

--- src/com/android/contacts/ContactsListActivity.java
+++ src/com/android/contacts/ContactsListActivity.java
@@ -1096,6 +1096,9 @@ public class ContactsListActivity extends ListActivity implements
             // Calling contact
             menu.add(0, MENU_ITEM_CALL, 0,
                     getString(R.string.menu_call));
+            // Edit Number before call
+            menu.add(0, MENU_ITEM_EDIT_BEFORE_CALL, 0,
+                    getString(R.string.recentCalls_editNumberBeforeCall));
             // Send SMS item
             menu.add(0, MENU_ITEM_SEND_SMS, 0, getString(R.string.menu_sendSMS));
         }
@@ -1139,6 +1142,32 @@ public class ContactsListActivity extends ListActivity implements
             case MENU_ITEM_CALL: {
                 callContact(cursor);
                 return true;
+            }
+            
+            case MENU_ITEM_EDIT_BEFORE_CALL: {
+            	 Uri numberUri = null;
+            	 String number = null;
+            	
+            	 final int contactId = cursor.getInt(SUMMARY_ID_COLUMN_INDEX);
+            	
+            	 Uri baseUri = ContentUris.withAppendedId(Contacts.CONTENT_URI, contactId);
+                 Uri dataUri = Uri.withAppendedPath(baseUri, Contacts.Data.CONTENT_DIRECTORY);
+
+                 Cursor c = getContentResolver().query(dataUri,
+                         new String[] {Phone._ID, Phone.NUMBER, Phone.IS_SUPER_PRIMARY},
+                         Data.MIMETYPE + "=?", new String[] {Phone.CONTENT_ITEM_TYPE}, null);
+                 
+                 if (c != null && c.moveToFirst()) {
+                	 number = c.getString(1);
+                 }
+                 c.close();
+
+            	 if(null != number) {
+	            	numberUri = Uri.fromParts("tel", number, null);
+	            	Intent intent = new Intent(Intent.ACTION_DIAL, numberUri);
+	            	startActivity(intent);
+            	}
+            	return true;
             }
 
             case MENU_ITEM_SEND_SMS: {
