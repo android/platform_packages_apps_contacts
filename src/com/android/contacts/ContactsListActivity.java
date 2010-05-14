a6a5b080298610558cc0f7ea6366627468bf749a
L&T Infotech <androidlntinfotech@gmail.com>
2010-05-14 10:18:19 +0530

    Defect 7261:Edit number before call
    Change-Id: I225fc0080ba7a3ca566b701bfe4315c5271b96a6

--- src/com/android/contacts/ContactsListActivity.java
+++ src/com/android/contacts/ContactsListActivity.java
@@ -1096,7 +1096,10 @@ public class ContactsListActivity extends ListActivity implements
             // Calling contact
             menu.add(0, MENU_ITEM_CALL, 0,
                     getString(R.string.menu_call));
-            // Send SMS item
+            // Edit before call
+            menu.add(0, MENU_ITEM_EDIT_BEFORE_CALL, 0,
+                    getString(R.string.recentCalls_editNumberBeforeCall));
+            //Send SMS item
             menu.add(0, MENU_ITEM_SEND_SMS, 0, getString(R.string.menu_sendSMS));
         }
 
@@ -1138,6 +1141,31 @@ public class ContactsListActivity extends ListActivity implements
 
             case MENU_ITEM_CALL: {
                 callContact(cursor);
+                return true;
+            }
+
+            case MENU_ITEM_EDIT_BEFORE_CALL: {
+                Uri numberUri = null;
+                String number = null;
+                
+                final int contactId = cursor.getInt(SUMMARY_ID_COLUMN_INDEX);
+            	Uri baseUri = ContentUris.withAppendedId(Contacts.CONTENT_URI, contactId);
+            	Uri dataUri = Uri.withAppendedPath(baseUri, Contacts.Data.CONTENT_DIRECTORY);
+
+            	Cursor c = getContentResolver().query(dataUri,
+                        new String[] {Phone._ID, Phone.NUMBER, Phone.IS_SUPER_PRIMARY},
+                        Data.MIMETYPE + "=?", new String[] {Phone.CONTENT_ITEM_TYPE}, null);
+            	
+            	if (c != null && c.moveToFirst()) {
+                    number = c.getString(1);
+                }
+                c.close();
+                 
+                if(null != number) {
+                	numberUri = Uri.fromParts("tel", number, null);
+                	Intent intent = new Intent(Intent.ACTION_DIAL, numberUri);
+                	startActivity(intent);
+            	}
                 return true;
             }
 
