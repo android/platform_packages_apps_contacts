c237c789782dc7206908a9c7bc246ef2771878ef
yuichiro fujiwara <yuichiro.fujiwara@sony.com>
2018-03-20 15:09:41 +0900

    When sending group mail via Gmail, Contacts application stops
    Root cause is because onActivityResult for RESULT_GROUP_ADD_MEMBER
    is processed for startActivityForResult with RESULT_SEND_TO_SELECTION.
    Define the following two Request codes as different values.
     - RESULT_SEND_TO_SELECTION
     - RESULT_GROUP_ADD_MEMBER
    
    Bug: Not yet
    Test: Manual
    Change-Id: I728347e01f6f4ba0f129b39448cfcd8580c59965

--- src/com/android/contacts/group/GroupUtil.java
+++ src/com/android/contacts/group/GroupUtil.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2018 Sony Mobile Communications Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +13,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * NOTE: This file has been modified by Sony Mobile Communications Inc.
+ * Modifications are licensed under the License.
  */
 
 package com.android.contacts.group;
@@ -60,7 +64,7 @@ public final class GroupUtil {
     public static final String ACTION_SWITCH_GROUP = "switchGroup";
     public static final String ACTION_UPDATE_GROUP = "updateGroup";
 
-    public static final int RESULT_SEND_TO_SELECTION = 100;
+    public static final int RESULT_SEND_TO_SELECTION = 200;
 
     // System IDs of FFC groups in Google accounts
     private static final Set<String> FFC_GROUPS =
